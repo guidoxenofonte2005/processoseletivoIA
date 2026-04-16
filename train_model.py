8011cc0e33d3f512e08e5aca79ec224373000faf
Guido <guidoxenofonte@gmail.com>
2026-04-16 17:34:24 -0300

    feat(main): add initial dataset setup
    Adição do dataset MNIST, normalização inicial dos dados de treino/teste
    (para ajustar a estabilidade e ajudar no treinamento) e ajuste nas
    imagens para transformação em vetores

--- train_model.py
+++ train_model.py
@@ -2,4 +2,10 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#insira seu código aqui
+(train_x, train_y), (test_x, test_y) = keras.datasets.mnist.load_data()
+
+train_x = train_x.as_type("float32") / 255
+train_y = train_y.as_type("float32") / 255
+
+train_x = train_x.reshape((train_x.shape[0], 28 * 28))
+test_x = test_x.reshape((train_x.shape[0], 28 * 28))
