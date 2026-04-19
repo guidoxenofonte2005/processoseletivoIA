5d3ba226b4e60820fa0c415cf5ef80a50a9bb034
Guido <guidoxenofonte@gmail.com>
2026-04-19 14:37:03 -0300

    feat(optimization): add optimize_model initial optimization techniques
    Adição de conversão do modelo e quantização sob o modelo exportado em
    formato .h5
    Futuros updates incluem testes, retreino com mais otimizações e
    possíveis melhorias adicionais

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,16 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+# carregamento inicial do modelo
+model = tf.keras.models.load_model("model.h5")
+
+# conversão do modelo
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# quantização
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+# salvamento
+tflite_model_path = "model.tflite"
+open(tflite_model_path, "wb").write(tflite_model)
